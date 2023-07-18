9c24cf6f733bcc9e6c9a0cadd96a78f8a1a6ec12
Mitesh Savsaviya <135125057+mitsav01@users.noreply.github.com>
2023-07-18 18:11:28 +0200

    Program showing mathematical functions in Python

---
+++ Mathematicalfunctions.py
@@ -0,0 +1,19 @@
+#Mathematical functions
+x= input("Enter value of X :")
+y= input("Enter value of Y :")
+x=int(x)
+y=int(y)
+
+print(f"Addition = {x+y}")
+
+print(f"Substraction = {x-y}")
+
+print(f"Multiplication = {x*y}")
+
+print(f"Divison = {x/y}"  )
+
+print(f"Exponent = {x**y}" )
+
+print(f"Modulus = {x%y}" )
+
+print(f"Floor divison = {x//y}" )
